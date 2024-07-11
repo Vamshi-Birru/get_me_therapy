f0befa37afdca5ace16c75299c273f6849a320a5
RDJ ironman <ironman053103@gmail.com>
2024-07-11 15:22:36 +0530

    locofy import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./Pages/home";
+import Login from './Pages/login';
+import Register from './Pages/register';
+import NotFound from './Pages/notFound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route element={<NotFound/>} />
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../Styles/register.css";
+function Register() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+}
+
+export default Register;
