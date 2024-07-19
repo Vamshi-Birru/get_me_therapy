36ae8c8065db01e94fc5755eeab574bd8634d8a4
RDJ ironman <ironman053103@gmail.com>
2024-07-19 16:32:13 +0530

    All done with the frontend

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import TrackingScreen from './Pages/trackingScreen';
 
 
 
@@ -23,7 +24,7 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path='/postLogin' element={<PostLogin/>}/>
-          
+          <Route path='trackingScreen' element={<TrackingScreen/>}/>
           
           <Route element={<NotFound/>} />
           </Routes>
