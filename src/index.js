190011fce37acbad74a1a9f852f97992cc3bfc25
Birru Vamshi <83230026+Vamshi-Birru@users.noreply.github.com>
2024-07-20 14:11:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import config from './config'; // Import the default export
-import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId={config.Google_Client_Id}>
-     <BrowserRouter>
+    
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
+  
   </GoogleOAuthProvider>
 );
 
